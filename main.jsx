461f6e0fda9ac692a8b7047669a6a80c510806cd
zepmasterbase <masterzep04@gmail.com>
2025-11-10 17:35:01 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,12 +1,20 @@
+/**
+ * RaceChain Main Entry
+ * ----------------------------
+ * This file boots the React app and sets up
+ * routing, styles, and global configuration.
+ */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import "./index.css"; // global styling (neon theme + animations)
 
-// React entry point
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
